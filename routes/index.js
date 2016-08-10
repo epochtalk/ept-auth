f78aca798376aef40ae4264bb45056831d69e8d1
Edward Kim <ed@slickage.com>
2016-08-09 15:13:01 -1000

    Admin recover route
    Added a new admin only recover route to allow admins to reset the
    password of any account. This route is protected by the ACLs system so
    any role that has the adminUsers.resetPassword permission can reset the
    account of any user.

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ module.exports = [
   require(path.normalize(__dirname + '/authenticate')),
   require(path.normalize(__dirname + '/username')),
   require(path.normalize(__dirname + '/email')),
+  require(path.normalize(__dirname + '/recover-html')),
   require(path.normalize(__dirname + '/recover')),
   require(path.normalize(__dirname + '/reset')),
   require(path.normalize(__dirname + '/checkResetToken')),
-  require(path.normalize(__dirname + '/recover-html'))
+  require(path.normalize(__dirname + '/adminRecover'))
 ];
